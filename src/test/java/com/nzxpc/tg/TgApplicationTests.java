8c56b705cd92ec2d6661cfcd2f74df76723375c3
梁涛 <320603976@qq.com>
2019-09-06 11:46:53 +0800

    初建项目

---
+++ src/test/java/com/nzxpc/tg/TgApplicationTests.java
@@ -0,0 +1,16 @@
+package com.nzxpc.tg;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class TgApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
