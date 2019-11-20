23696b203f7ee7cd226bcb90a87d2ed414fa25e6
梁涛 <320603976@qq.com>
2019-11-20 16:25:18 +0800

    xx

--- src/main/java/com/nzxpc/tg/controller/HomeController.java
+++ src/main/java/com/nzxpc/tg/controller/HomeController.java
@@ -1,6 +1,19 @@
 package com.nzxpc.tg.controller;
 
+import org.springframework.web.bind.annotation.*;
 
+import java.util.ArrayList;
+import java.util.List;
+
+@RestController
+@RequestMapping(path = "/home")
 public class HomeController {
-
+    @RequestMapping("login")
+    public List<Integer> login() {
+        List<Integer>list=new ArrayList<>();
+        list.add(1);
+        list.add(2);
+        list.add(3);
+        return list;
+    }
 }
