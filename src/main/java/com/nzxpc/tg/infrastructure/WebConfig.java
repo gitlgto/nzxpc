aa29a4a126615330c02b41edcb374afb5de7bac6
梁涛 <320603976@qq.com>
2019-11-21 08:39:52 +0800

    跨域问题

---
+++ src/main/java/com/nzxpc/tg/infrastructure/WebConfig.java
@@ -0,0 +1,8 @@
+package com.nzxpc.tg.infrastructure;
+
+import com.nzxpc.handler.web.WebAppConfig;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class WebConfig extends WebAppConfig {
+}
